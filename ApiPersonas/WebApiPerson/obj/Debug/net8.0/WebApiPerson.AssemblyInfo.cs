0cd12147b5f7ab94446da2fcd69683e7ba6cac4d
Bryan Pazmino <bryanpazmi1977@gmail.com>
2025-03-31 16:54:43 -0500

    borado .vs

--- ApiPersonas/WebApiPerson/obj/Debug/net8.0/WebApiPerson.AssemblyInfo.cs
+++ ApiPersonas/WebApiPerson/obj/Debug/net8.0/WebApiPerson.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiPerson")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69f2071578152d31323ca41e87c3d78cfc55fde0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b2da7f97f9ec5db58b80537274334080b3378fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiPerson")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiPerson")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
